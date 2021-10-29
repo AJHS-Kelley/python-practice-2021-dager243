d724f8f840de6064d7945dac3b029fe5fb4b68a7
Beau DP <s12469680@students.duvalschools.org>
2021-10-29 14:40:06 -0400

    version 0.1
    Added three different types of data integers, floating point, and string.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# variables practice, Beau Diaz-Perez, 10-29-21 2:23, version 0.0
+# variables practice, Beau Diaz-Perez, 10-29-21 2:37, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 5 # INTEGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "How can I be defated by the likes of you."
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
